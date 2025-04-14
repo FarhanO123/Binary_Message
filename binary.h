d79afdde3c8d9cf898a0c09685c036cb7f53454b
Farhan Omane <faromane21@gmail.com>
2025-04-14 16:36:42 -0400

    Updated typeDef and some parameters to run

--- binary.h
+++ binary.h
@@ -5,7 +5,17 @@
 
 
 
+typedef struct {
 
+    char character; // character to be encoded 
+    char binary[9]; // there is only 8 bits in a byte and we need a null terminator
+
+} BinaryMapping;
+
+
+void LoadBinaryMapping(const char *filename);
+void ConvertToBinary(const char *input, char *output);
+void FreeBinaryMapping();
 
 
 
